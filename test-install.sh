829db95db3d9dc6c88ac01fe1b4137e3956f457d
fsamreen <aziaf2002@yahoo.com>
2017-07-07 14:18:59 +0100

    Update test-install.sh

--- test-install.sh
+++ test-install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 apt-get update
 apt-get install gfortran -y
+apt-get install gcc -y
+apt get install g++ -y
+which fortran
+which gcc
+which cpp
+gcc --version
+gfortran --version
+mkdir Build_WRF
+mkdir TESTS
+cd Build_WRF
