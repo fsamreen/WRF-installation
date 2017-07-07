7343da8b8aa7261d9edbb4c1d7a38285feb1dc48
fsamreen <aziaf2002@yahoo.com>
2017-07-07 15:07:49 +0100

    Create test3.sh

---
+++ test3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get install csh -y
+apt-get install perl
+cd ~/TESTS
+#Test #5:csh In the command line, type:
+./TEST_csh.csh
+#The result should be: SUCCESS csh test
+#Test #6:perl In the command line, type:
+./TEST_perl.pl
+#The result should be: SUCCESS perl test
+#Test #7:sh In the command line, type:
+./TEST_sh.sh
+#The result should be: SUCCESS sh test
