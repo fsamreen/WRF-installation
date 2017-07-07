c8134aec900e7fef9c4fe8828549c7a4121238c2
fsamreen <aziaf2002@yahoo.com>
2017-07-07 15:19:11 +0100

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -5,3 +5,20 @@ wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/netc
 wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/jasper-1.900.1.tar.gz
 wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/libpng-1.2.50.tar.gz
 wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/zlib-1.2.7.tar.gz
+tsch
+setenv DIR ~/Build_WRF/LIBRARIES
+setenv CC gcc
+setenv CXX g++
+setenv FC gfortran
+setenv FCFLAGS -m64
+setenv F77 gfortran
+setenv FFLAGS -m64
+tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
+cd netcdf-4.1.3
+./configure --prefix=$DIR/netcdf --disable-dap \
+     --disable-netcdf-4 --disable-shared
+make
+make install
+setenv PATH $DIR/netcdf/bin:$PATH
+setenv NETCDF $DIR/netcdf
+cd ..
