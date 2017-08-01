7a1c72ff1a409f57c6f5ead75527e0c8042ff5bb
fsamreen <aziaf2002@yahoo.com>
2017-08-01 13:53:46 +0100

    Create netcdf-step2.sh

---
+++ netcdf-step2.sh
@@ -0,0 +1,8 @@
+cd ~/Build_WRF/LIBRARIES
+tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
+cd netcdf-4.1.3
+./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
+apt-get install make -y
+make
+make install
+cd ..
